c07ae2cbffee3030257dca8ce47870510669af82
Francisco Villar Salvador <franvlive@gmail.com>
2023-02-28 09:27:23 +0100

    Add initial State for the app

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -5,14 +5,23 @@ import { initialFilms } from "../features/films/filmsSlice";
 const useFetch = () => {
   const [searchValue, setSearchValue] = useState("");
 
-  const url = `https://api.themoviedb.org/3/search/movie?api_key=7cdd6813e009397c594758fe7bce7b47&language=en-US&query=${searchValue}&append_to_response=videos`;
+  const initialURL =
+    "https://api.themoviedb.org/3/movie/top_rated/?api_key=7cdd6813e009397c594758fe7bce7b47&language=en-US";
+
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=7cdd6813e009397c594758fe7bce7b47&language=en-US&query=${searchValue}`;
 
   const dispatch = useDispatch();
 
   useEffect(() => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((res) => dispatch(initialFilms(res.results)));
+    if (searchValue !== "") {
+      fetch(url)
+        .then((res) => res.json())
+        .then((res) => dispatch(initialFilms(res.results)));
+    } else {
+      fetch(initialURL)
+        .then((res) => res.json())
+        .then((res) => dispatch(initialFilms(res.results)));
+    }
   }, [url]);
 
   return { setSearchValue };
